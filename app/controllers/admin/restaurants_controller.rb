f90b6fb174946904479bf4454d1b1e843dc21904
yungcheng <yungcheng111023@gmail.com>
2017-12-28 15:01:24 +0800

    add restaurant show action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -21,6 +21,10 @@ class Admin::RestaurantsController < ApplicationController
     end
   end
 
+   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
 
   def restaurant_params
