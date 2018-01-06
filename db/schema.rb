bafd6ce16445ffd8384ab754c3cb5be9de9105fb
yungcheng <yungcheng111023@gmail.com>
2018-01-06 13:39:52 +0800

    add attributes to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101141200) do
+ActiveRecord::Schema.define(version: 20180106053354) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,7 +26,13 @@ ActiveRecord::Schema.define(version: 20180101141200) do
     t.datetime "updated_at", null: false
   end
 
-  
+  create_table "commets", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
