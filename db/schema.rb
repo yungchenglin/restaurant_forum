8db2af124e344cc18cc5a21b16ed9a83633f7f05
yungcheng <yungcheng111023@gmail.com>
2018-01-06 23:44:29 +0800

    add column image to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106053354) do
+ActiveRecord::Schema.define(version: 20180106154112) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180106053354) do
     t.string "name"
     t.text "intro"
     t.string "avatar"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
