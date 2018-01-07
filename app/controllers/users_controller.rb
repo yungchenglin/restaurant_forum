dd1e702da703a672f2bb781993179557a78dd588
yungcheng <yungcheng111023@gmail.com>
2018-01-07 21:05:48 +0800

    add user name to registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,26 @@
 class UsersController < ApplicationController
+   before_action :set_user, only: [:show, :edit, :update]
 
-   def show
-       @user = User.find(params[:id])  
+   def show 
    end
 
    def edit
-       @user = User.find(params[:id])
         unless @user == current_user
           redirect_to user_path(@user)
         end 
    end
 
    def update
-       @user = User.find(params[:id])
        @user.update(user_params)
        redirect_to user_path(@user)  
    end
 
 
  private
+
+  def set_user
+       @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :intro, :avatar)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
+  validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
   has_many :comments
