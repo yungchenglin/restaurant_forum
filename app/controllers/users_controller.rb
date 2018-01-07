bfb79003b986b0bdc74f42bbdadd03f7fb1aa73d
yungcheng <yungcheng111023@gmail.com>
2018-01-08 00:19:52 +0800

    styled profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
    before_action :set_user, only: [:show, :edit, :update]
 
    def show 
-       @commented_restaurants = @user.restaurants.uniq
+       @commented_restaurants = @user.restaurants
    end
 
    def edit
