a350de91fb0f9f48078f0f4898c2053e8f16998c
yungcheng <yungcheng111023@gmail.com>
2018-01-14 08:27:45 +0800

    setup route for ranking page

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
 
     collection do
+      
       get :feeds
+
+      get :ranking
+
     end
 
     member do
+
       get :dashboard
     
       post :favorite
@@ -17,6 +22,7 @@ Rails.application.routes.draw do
 
       post :like
       post :unlike
+
     end
 
   end
